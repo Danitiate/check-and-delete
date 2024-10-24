4d481bddcd45b50aa6333a00f759181fffbc494f
Daniel Dutta <daniel.e.dutta@gmail.com>
2024-10-24 15:53:17 +0200

    - Remove all children of a parent listItem - Fix check-and-delete button not being clickable when populated with children - Fix widget render issue in preview mode - Fix check-and-delete functionality in preview mode (WIP)

--- src/plugins/check-and-delete-plugin/features/checkAndDeleteDecoratorWidget.ts
+++ src/plugins/check-and-delete-plugin/features/checkAndDeleteDecoratorWidget.ts
@@ -5,10 +5,32 @@ export class CheckAndDeleteDecoratorWidget extends WidgetType {
 		const span = document.createElement("span");
 		span.className = "check-and-delete-task-button"
 		span.onclick = () => {
+			let toBeRemovedElements = [];
 			const parent = span.parentNode as HTMLElement;
-			parent.remove();
+			toBeRemovedElements.push(parent)
+			const parentIndentLevel = this.extractIndentLevelFromClassname(parent.className);
+			let nextSibling = parent.nextElementSibling
+			let nextSiblingIndentLevel = this.extractIndentLevelFromClassname(nextSibling?.className ?? "");
+			while(nextSibling && nextSiblingIndentLevel > parentIndentLevel) {
+				toBeRemovedElements.push(nextSibling)
+				nextSibling = nextSibling.nextElementSibling
+				nextSiblingIndentLevel = this.extractIndentLevelFromClassname(nextSibling?.className ?? "");
+			}
+			toBeRemovedElements.forEach(element => {
+				element.remove();
+			})
 		}
 		return span;
+	}
+
+	private extractIndentLevelFromClassname(classname: string): number {
+		const match = classname.match(/HyperMD-list-line-(\d+)/);
+		if (match) {
+			return Number(match[1])
+		}
+		else {
+			return 0;
+		}
 	}
 }
 
--- src/plugins/check-and-delete-plugin/features/checkAndDeletePostProcessor.ts
+++ src/plugins/check-and-delete-plugin/features/checkAndDeletePostProcessor.ts
@@ -1,17 +1,36 @@
-import { Plugin } from "obsidian";
+import { MarkdownEditView, MarkdownView, Plugin } from "obsidian";
+import { EditorView } from '@codemirror/view';
 
-function renderCheckAndDeleteInMarkdown(element: HTMLElement) {
+function renderCheckAndDeleteInMarkdown(plugin: Plugin, element: HTMLElement) {
 	const listItems = element.findAll("li");
 	for (let listItem of listItems) {
 		if (/^\s*\([Xx]\)\s/.test(listItem.textContent ?? "")) {
-			listItem.innerHTML = listItem.innerHTML.replace("list-bullet", "check-and-delete-task-button")
-			listItem.innerHTML = listItem.innerHTML.replace("<p>(x) ", "<p>")
+			listItem.innerHTML = listItem.innerHTML.replace(/class=\"list-bullet\"/g, "class=\"check-and-delete-task-button\"")
+			listItem.innerHTML = listItem.innerHTML.replace(/<p>\([xX]\)\s/g, "<p>")
+			listItem.innerHTML = listItem.innerHTML.replace(/<\/span>\([xX]\)\s/g, "</span>")
 
 			let span = listItem.firstChild as HTMLElement;
-			span.onClickEvent(() => {
-				const parent = span.parentNode as HTMLElement;
-				parent.remove();
-				// TODO: Remove text from Editor
+			span.onClickEvent(async (event) => {
+				// Remove item from file / editor
+				const activeFile = plugin.app.workspace.getActiveFile();
+				const activeView = plugin.app.workspace.getActiveViewOfType(MarkdownView);
+				const editorView = (activeView?.editor as any).cm as EditorView;
+				if(activeFile && editorView && listItem.textContent) {
+					let fileContent = await plugin.app.vault.read(activeFile);
+					const targetString = "- (x) " + listItem.textContent.trim();
+					const state = editorView.state;
+					for(let i = 1; i <= state.doc.lines; i++) {
+						const line = state.doc.line(i);
+						if (targetString == line.text.trim()) {
+							const firstPart = fileContent.substring(0, line.from)
+							const lastPart = fileContent.substring(line.to + 1)
+							fileContent = firstPart + lastPart;
+							await this.app.vault.modify(activeFile, fileContent);
+							listItem.remove(); // Removes item from render
+							break;
+						}
+					}
+				}
 			})
 		}
 	}
@@ -19,7 +38,7 @@ function renderCheckAndDeleteInMarkdown(element: HTMLElement) {
 
 function addCheckAndDeletePostProcessor(plugin: Plugin) {
 	plugin.registerMarkdownPostProcessor((element, context) => {
-		renderCheckAndDeleteInMarkdown(element);
+		renderCheckAndDeleteInMarkdown(plugin, element);
 	});
 }
 
