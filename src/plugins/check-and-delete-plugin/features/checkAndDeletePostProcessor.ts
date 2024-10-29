0233ed466f2f0703cbc98fe958fc8c77c41e3d3f
Daniel Dutta <daniel.e.dutta@gmail.com>
2024-10-29 15:43:35 +0100

    - Updated checkAndDeletePostProcessor logic     - Allow post processor to delete nested check-and-delete lists     - Fix issue were every list-bullet was rendered as a check-and-delete item despite not having "- (x) " prefix

--- src/plugins/check-and-delete-plugin/features/checkAndDeletePostProcessor.ts
+++ src/plugins/check-and-delete-plugin/features/checkAndDeletePostProcessor.ts
@@ -1,45 +1,105 @@
 import { MarkdownEditView, MarkdownView, Plugin } from "obsidian";
 import { EditorView } from '@codemirror/view';
 
-function renderCheckAndDeleteInMarkdown(plugin: Plugin, element: HTMLElement) {
-	const listItems = element.findAll("li");
-	for (let listItem of listItems) {
-		if (/^\s*\([Xx]\)\s/.test(listItem.textContent ?? "")) {
-			listItem.innerHTML = listItem.innerHTML.replace(/class=\"list-bullet\"/g, "class=\"check-and-delete-task-button\"")
-			listItem.innerHTML = listItem.innerHTML.replace(/<p>\([xX]\)\s/g, "<p>")
-			listItem.innerHTML = listItem.innerHTML.replace(/<\/span>\([xX]\)\s/g, "</span>")
-
-			let span = listItem.firstChild as HTMLElement;
-			span.onClickEvent(async (event) => {
-				// Remove item from file / editor
-				const activeFile = plugin.app.workspace.getActiveFile();
-				const activeView = plugin.app.workspace.getActiveViewOfType(MarkdownView);
-				const editorView = (activeView?.editor as any).cm as EditorView;
-				if(activeFile && editorView && listItem.textContent) {
-					let fileContent = await plugin.app.vault.read(activeFile);
-					const targetString = "- (x) " + listItem.textContent.trim();
-					const state = editorView.state;
-					for(let i = 1; i <= state.doc.lines; i++) {
-						const line = state.doc.line(i);
-						if (targetString == line.text.trim()) {
-							const firstPart = fileContent.substring(0, line.from)
-							const lastPart = fileContent.substring(line.to + 1)
-							fileContent = firstPart + lastPart;
-							await this.app.vault.modify(activeFile, fileContent);
-							listItem.remove(); // Removes item from render
-							break;
-						}
-					}
-				}
-			})
-		}
-	}
-}
-
 function addCheckAndDeletePostProcessor(plugin: Plugin) {
 	plugin.registerMarkdownPostProcessor((element, context) => {
 		renderCheckAndDeleteInMarkdown(plugin, element);
 	});
+}
+
+function renderCheckAndDeleteInMarkdown(plugin: Plugin, element: HTMLElement) {
+	if(element.className == "el-ul") {
+		iterateCheckAndDeleteChildren(element, plugin)
+	}
+}
+
+function iterateCheckAndDeleteChildren(element: HTMLElement, plugin: Plugin) {
+	const children = element.childNodes
+	children.forEach(child => {
+		if(child instanceof HTMLLIElement && /^\s*\([Xx]\)\s/.test(child.textContent ?? "")) {
+			// ListItem contains span and text items -> Recursively iterate children if prefixed with "(x)"
+			iterateCheckAndDeleteChildren(child, plugin)
+		}
+		else if (child instanceof HTMLSpanElement && child.className == "list-bullet") {
+			// If a span element is present in the listItem, it must be a check-and-delete-button
+			child.className = "check-and-delete-task-button";
+			child.onClickEvent(() => {
+				checkAndDeleteHandler(element, plugin)
+			})
+		}
+		else if(child instanceof HTMLUListElement) {
+			// UnorderedList contains listItems -> Recursively iterate children
+			iterateCheckAndDeleteChildren(child, plugin)
+		}
+		else if(child instanceof Text) {
+			// Remove prefix "(x) " from rendered text
+			child.data = child.data.replace(/^\([xX]\)\s/, "")
+		}
+		else if(child instanceof HTMLParagraphElement) {
+			// Remove prefix "(x) " from rendered text
+			child.innerHTML = child.innerHTML.replace(/^\([xX]\)\s/, "")
+		}
+	})
+}
+
+function checkAndDeleteHandler(listItem: HTMLElement, plugin: Plugin) {
+	deleteElementFromPreview(listItem);
+	deleteElementFromEditor(listItem, plugin)
+}
+
+function deleteElementFromPreview(element: HTMLElement) {
+	element.remove();
+}
+
+async function deleteElementFromEditor(element: HTMLElement, plugin: Plugin) {
+	const activeFile = plugin.app.workspace.getActiveFile();
+	const activeView = plugin.app.workspace.getActiveViewOfType(MarkdownView);
+	const editorView = (activeView?.editor as any).cm as EditorView;
+	const state = editorView.state;
+	
+	const elementText = getElementText(element)
+	if (activeFile && editorView && elementText) {
+		const fileContent = await plugin.app.vault.read(activeFile);
+		const indexOfDeletedLine = fileContent.indexOf("- (x) " + elementText);
+		const firstLineToDelete = state.doc.lineAt(indexOfDeletedLine);
+		const firstLineToDeleteIndentLevelMatch = firstLineToDelete.text.match(/^\t*/);
+		const firstLineToDeleteIndentLevel = firstLineToDeleteIndentLevelMatch![0].length;
+		let lastPositionToRemove = firstLineToDelete.to + 1;
+		for (let i = firstLineToDelete.number + 1; i < state.doc.lines; i++) {
+			const lastLineToDelete = state.doc.line(i)
+			const lastLineToDeleteIndentLevelMatch = lastLineToDelete.text.match(/^\t*/);
+			const lastLineToDeleteIndentLevel = lastLineToDeleteIndentLevelMatch![0].length;
+			if (lastLineToDeleteIndentLevel > firstLineToDeleteIndentLevel) {
+				lastPositionToRemove = lastLineToDelete.to + 1;
+			}
+			else {
+				break;
+			}
+		}
+
+		let firstPart = fileContent.substring(0, firstLineToDelete.from);
+		let lastPart = fileContent.substring(lastPositionToRemove)
+		await this.app.vault.modify(activeFile, firstPart + lastPart);
+	}
+}
+
+function getElementText(element: HTMLElement): string {
+	let elementText = "";
+	const children = element.childNodes;
+	for(let i = 0; i < children.length; i++) {
+		const child = children.item(i)
+		// Text element could either be a paragraph element or raw text
+		if (child instanceof Text && !child.nextElementSibling) {
+			elementText = child.data;
+			break;
+		}
+		else if(child instanceof HTMLParagraphElement && child.innerText) {
+			elementText = child.innerText;
+			break;
+		}
+	}
+
+	return elementText;
 }
 
 export default addCheckAndDeletePostProcessor;
