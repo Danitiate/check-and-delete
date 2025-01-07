38d3cb29d123aba2087c40da565e01841f465429
Daniel Dutta <daniel.e.dutta@gmail.com>
2025-01-07 13:58:28 +0000

    OnClick functionality for delete buttons in delete-internal-link-modal - Remove item from list when deleting a single item - Delete linked note by utilizing `app.vault.delete` - Updated delete-internal-link-modal styling     - Make sure long pathnames are trunctated     - Prevent seeing rows scrolling behind user-control

--- src/plugins/check-and-delete-plugin/features/deleteInternalLinksDialog.ts
+++ src/plugins/check-and-delete-plugin/features/deleteInternalLinksDialog.ts
@@ -1,4 +1,4 @@
-import { App, Modal, Setting, TFile } from 'obsidian';
+import { App, Modal, Notice, TFile } from 'obsidian';
 
 export function activateDeleteInternalLinksDialogView(linkedFiles: TFile[]) {
     if (linkedFiles.length > 0) {
@@ -9,41 +9,88 @@ export function activateDeleteInternalLinksDialogView(linkedFiles: TFile[]) {
 export class DeleteInternalLinkModal extends Modal {
   constructor(app: App, linkedFiles: TFile[]) {
     super(app);
-    this.titleEl.setText(`Delete ${linkedFiles.length} linked file${linkedFiles.length > 1 ? 's' : ''}`)
+    this.modalEl.classList.add("delete-internal-link-modal")
+    this.titleEl.setText(`Delete ${linkedFiles.length} linked file${linkedFiles.length > 1 ? 's' : ''}?`)
 
-    linkedFiles.forEach(linkedFile => {
-        new Setting(this.contentEl)
-            .setName(linkedFile.path)
-            .addButton((button) => {
-                button.setButtonText("Delete")
-                      .setClass("delete-internal-link-button")
-            })
-    })
+    const modalContent = document.createElement("div");
+    this.addLinkedFilesRows(linkedFiles, modalContent);
 
-    const userControls = document.createElement("div");
-    userControls.className = "delete-internal-links-user-control";
+    const userControls = modalContent.createDiv({ cls: "delete-internal-links-user-control" });
     this.addCancelButton(userControls)
     if (linkedFiles.length > 1) {
-        this.addDeleteAllButton(userControls)
+        this.addDeleteAllButton(userControls, linkedFiles)
     }
 
-    this.contentEl.appendChild(userControls);
+    this.contentEl.appendChild(modalContent);
   }
 
-  private addCancelButton(userControls: HTMLDivElement) {
-      userControls.createEl("button", { text: "Close"}, (cancelButton) => {
-          cancelButton.addEventListener("click", () => {
-              this.close();
-          })
-      })
-  }
+    private addLinkedFilesRows(linkedFiles: TFile[], modalContent: HTMLDivElement) {
+        linkedFiles.forEach(linkedFile => {
+            const modalContentRow = modalContent.createDiv({ cls: "delete-internal-link-item-row" });
+            modalContentRow.createDiv({ text: linkedFile.path, cls: "delete-internal-link-item-row-text" });
+            this.addDeleteButton(modalContentRow, linkedFiles, linkedFile);
+        });
+    }
+
+    private addDeleteButton(modalContentRow: HTMLDivElement, linkedFiles: TFile[], linkedFile: TFile) {
+        modalContentRow.createEl("button", { text: "Delete", cls: "delete-internal-link-button"}, (deleteButton) => {
+            deleteButton.addEventListener("click", () => {
+                this.deleteFile(linkedFile)
+                    .then(() => {
+                        new Notice("Deleted: " + linkedFile.path);
+                        linkedFiles.remove(linkedFile);
+                        modalContentRow.remove();
+                        if (linkedFiles.length == 0) {
+                            this.close();
+                        }
+                    })
+                    .catch(error => {
+                        new Notice("Failed delete: " + error, 10000)
+                    })
+            })
+        })
+    }
+
+    private addCancelButton(userControls: HTMLDivElement) {
+        userControls.createEl("button", { text: "Close"}, (cancelButton) => {
+            cancelButton.addEventListener("click", () => {
+                this.close();
+            })
+        })
+    }
   
-  private addDeleteAllButton(userControls: HTMLDivElement) {
-      userControls.createEl("button", { text: "Delete All", cls: "delete-internal-link-button"}, (deleteAllButton) => {
-          deleteAllButton.addEventListener("click", () => {
-              this.close();
-          })
-      })
-  }
+    private addDeleteAllButton(userControls: HTMLDivElement, linkedFiles: TFile[]) {
+        userControls.createEl("button", { text: "Delete All", cls: "delete-internal-link-button"}, (deleteAllButton) => {
+            deleteAllButton.addEventListener("click", () => {
+                this.deleteFiles(linkedFiles).then(failedDeletes => {
+                    if (failedDeletes.length < linkedFiles.length) {
+                        new Notice(`Successfully deleted ${linkedFiles.length - failedDeletes.length} files`)
+                    }
+                    if (failedDeletes.length > 0) {
+                        new Notice(`Failed to delete the following files:\n- ${failedDeletes.join("\n- ")}`, 10000)
+                    }
+                });
+            })
+        })
+    }
+
+    private async deleteFiles(linkedFiles: TFile[]): Promise<string[]> {
+        new Notice(`Deleting ${linkedFiles.length} files...`);
+        const failedDeletes: string[] = [];
+        await Promise.all(
+            linkedFiles.map(async linkedFile => {
+                await this.deleteFile(linkedFile)
+                    .catch(error => {
+                        failedDeletes.push(linkedFile.name);
+                    })
+            }));
+
+        return failedDeletes;
+    }
+
+    private async deleteFile(linkedFile: TFile): Promise<void> {
+        // TODO: trash or delete based on settings
+        return this.app.vault.delete(linkedFile)
+    }
 }
 
